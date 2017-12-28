ed2a0e0f834129466b5ee48d845b5c17e3d077fa
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-28 16:16:33 +0600

    not need to override this constructor

--- client/src/main/java/tech/lapsa/esbd/connection/ConnectionException.java
+++ client/src/main/java/tech/lapsa/esbd/connection/ConnectionException.java
@@ -6,11 +6,6 @@ public class ConnectionException extends RuntimeException {
     public ConnectionException() {
     }
 
-    public ConnectionException(final String message, final Throwable cause, final boolean enableSuppression,
-	    final boolean writableStackTrace) {
-	super(message, cause, enableSuppression, writableStackTrace);
-    }
-
     public ConnectionException(final String message, final Throwable cause) {
 	super(message, cause);
     }
